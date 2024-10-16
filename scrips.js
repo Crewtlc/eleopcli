178689ca17057b4fe3c296bc870e67ba6499f6e6
Crewtlc <longhourswaltair@gmail.com>
2024-10-16 11:24:12 +0530

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -1,6 +1,3 @@
-function login() {
-  // Redirect to login page or show login form
-  alert('Login button clicked!');
-  // For example, redirect to a login page:
-  // window.location.href = 'https://crewtlc.github.io/cliforms/';
-}
+document.getElementById('Click here').addEventListener('click', function() {
+    window.location.href = "https://crewtlc.github.io/Lpform/"; 
+});
