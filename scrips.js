d081fc7be477ff8fd9ee2f96bf354b77702a688f
Crewtlc <longhourswaltair@gmail.com>
2024-10-16 11:18:05 +0530

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,6 @@
+function login() {
+  // Redirect to login page or show login form
+  alert('Login button clicked!');
+  // For example, redirect to a login page:
+  // window.location.href = 'login.html';
+}
